6ba0c1cc7fdb91a4215b9dc831b2d04b3e18200a
extremjaws <88508940+extremjaws@users.noreply.github.com>
2021-11-27 16:10:07 -0700

    gsertvnpioh a

--- Assets/menuButtons.cs
+++ Assets/menuButtons.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class menuButtons : MonoBehaviour
 {
+    public GameObject mainpanel;
+    public GameObject settingsPanel;
+    public GameObject selectorPanel;
     public void quit()
     {
         Application.Quit();
+    }
+    public void settings()
+    {
+        mainpanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+    public void noMoreSettings()
+    {
+        settingsPanel.SetActive(false);
+        mainpanel.SetActive(true);
     }
 }

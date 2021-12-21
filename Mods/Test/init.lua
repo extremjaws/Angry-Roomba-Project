1bdd0ad09215d8bb3c7922a88454495ac6351c3b
Owenknowsbest <78163015+Owenknowsbest@users.noreply.github.com>
2021-12-20 19:59:14 -0700

    Test

--- Mods/Test/init.lua
+++ Mods/Test/init.lua
@@ -1,7 +1,16 @@
+local roombaspawns = Objects.FindRoombas()
 function CommandHandler(args)
 	if args[2] == "givehammer" then
 		if Console.CheatsActive() then
 			Objects.SpawnObject(0, Objects.FindPlayer()[1], Objects.FindPlayer()[2], Objects.FindPlayer()[3])
+		end
+	elseif args[2] == "roombahell" then
+		if Console.CheatsActive() then
+			for i=1, 100 / #roombaspawns do
+				for i, v in pairs(roombaspawns) do
+					Objects.SpawnObject(1, v[1], v[2], v[3])
+				end
+			end
 		end
 	end
 end

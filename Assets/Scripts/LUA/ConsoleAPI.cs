de4c79c751021c15eb269f33b05bc0aeb5760e55
Owenknowsbest <78163015+Owenknowsbest@users.noreply.github.com>
2021-12-20 19:22:24 -0700

    Added Console.CheatsActive

--- Assets/Scripts/LUA/ConsoleAPI.cs
+++ Assets/Scripts/LUA/ConsoleAPI.cs
@@ -10,6 +10,7 @@ public class ConsoleAPI : LuaAPIBase
     protected override void InitialiseAPITable()
     {
         m_ApiTable["RunCommand"] = (System.Func<string, bool>)(Lua_RunCommand);
+        m_ApiTable["CheatsActive"] = (System.Func<bool>)(Lua_CheatsActive);
     }
     [LuaApiFunction(name = "RunCommand", description = "Run console command", codeExample = "Console.RunCommand(\"cheats\") -- enable cheats")]
     private bool Lua_RunCommand(string command)
@@ -17,5 +18,10 @@ public class ConsoleAPI : LuaAPIBase
         MonoBehaviour.FindObjectOfType<console>().textEntry.GetComponent<TMP_InputField>().text = command;
         MonoBehaviour.FindObjectOfType<console>().enterCommand();
         return true;
+    }
+    [LuaApiFunction(name = "CheatsActive", description = "Get if cheats are active", codeExample = "if Console.CheatsActive() then Console.RunCommand(\"cheats\") end -- disable cheats")]
+    private bool Lua_CheatsActive()
+    {
+        return MonoBehaviour.FindObjectOfType<console>().cheats;
     }
 }

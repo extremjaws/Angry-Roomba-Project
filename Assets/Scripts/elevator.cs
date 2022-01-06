197e5d83495a12b27a231cea56cb4fee68e9f3e1
extremjaws <88508940+extremjaws@users.noreply.github.com>
2022-01-06 16:07:15 -0700

    started working on additive scene loading

--- Assets/Scripts/elevator.cs
+++ Assets/Scripts/elevator.cs
@@ -1,38 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class elevator : MonoBehaviour
 {
     public string levelToLoad;
     public bool isStart;
     public GameObject[] roombasToActivate;
-    private void Start()
-    {
-        if (isStart)
-        {
-            StartCoroutine("startLevel");
-        }
-    }
+    public GameObject elevatorTrigger;
     public void goToNextLevel()
     {
-        StartCoroutine("goUp");
+        goUp();
     }
-    IEnumerator goUp()
+    async void goUp()
     {
         GetComponent<Animator>().SetTrigger("elevatorUp");
-        yield return new WaitForSeconds(12);
-        FindObjectOfType<loadingAnimPlayer>().loadnext(levelToLoad);    
+        //yield return new WaitForSeconds(12);
+        await Task.Delay(15000);
+        SceneManager.LoadSceneAsync(levelToLoad, LoadSceneMode.Additive); 
     }
-    IEnumerator startLevel()
-    {
-        FindObjectOfType<PlayerMovement>().elevatorMotion = true;
-        GetComponent<Animator>().SetTrigger("elevatorStart");
-        yield return new WaitForSeconds(12);
-        FindObjectOfType<PlayerMovement>().elevatorMotion = false;
-        foreach (GameObject roomba in roombasToActivate)
-        {
-            roomba.GetComponent<controller>().enabled = true;
-        }
-    }
+    //async void startLevel()
+    //{
+    //    FindObjectOfType<PlayerMovement>().elevatorMotion = true;
+    //    GetComponent<Animator>().SetTrigger("elevatorStart");
+    //    //yield return new WaitForSeconds(12);
+    //    await Task.Delay(15000);
+    //    FindObjectOfType<PlayerMovement>().elevatorMotion = false;
+    //    foreach (GameObject roomba in roombasToActivate)
+    //    {
+    //        roomba.GetComponent<controller>().enabled = true;
+    //    }
+    //}
 }

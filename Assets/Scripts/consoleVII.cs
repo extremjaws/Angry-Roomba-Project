7dfb712f2572ef00557121a38ec1306ec0b8b214
Owenknowsbest <78163015+Owenknowsbest@users.noreply.github.com>
2022-06-26 15:30:31 -0600

    New console implementation

--- Assets/Scripts/consoleVII.cs
+++ Assets/Scripts/consoleVII.cs
@@ -11,6 +11,7 @@ public class consoleVII : MonoBehaviour
     public static string log = "";
     public static string input = "";
     public static bool cheats = false;
+    public Dictionary<string, string> binds;
     Vector2 scrollpos;
 
     public void Awake()
@@ -24,6 +25,7 @@ public class consoleVII : MonoBehaviour
             // Only one should exist
             Destroy(this);
         }
+        binds = new Dictionary<string, string>();
     }
 
     public void Update()
@@ -34,6 +36,16 @@ public class consoleVII : MonoBehaviour
             input = "";
             Cursor.lockState = (open ? CursorLockMode.None : CursorLockMode.Locked);
             Cursor.visible = open;
+        }
+        if(!open)
+        {
+            foreach(string bind in binds.Keys)
+            {
+                if(Input.GetKeyDown(bind))
+                {
+                    RunCommand(binds[bind]);
+                }
+            }
         }
     }
 
@@ -64,15 +76,42 @@ public class consoleVII : MonoBehaviour
         }
     }
 
+    [cmd("echo", description = "seemingly obligitory command for any console. guess what it does!")]
     void print(string text, string end = "\n")
     {
         log += text;
         log += end;
+        scrollpos = new Vector2(0, log.Split('\n').Length * 15 - 395);
+    }
+    
+    string[] CommandSplit(string input)
+    {
+        List<string> output = new List<string>();
+        string temp = "";
+        bool str = false;
+        foreach (char character in input)
+        {
+            if (character == ' ' && !str)
+            {
+                output.Add(temp);
+                temp = "";
+            }
+            else if(character == '\"')
+            {
+                str = !str;
+            }
+            else
+            {
+                temp += character;
+            }
+        }
+        output.Add(temp);
+        return output.ToArray();
     }
 
     void RunCommand(string arg)
     {
-        string[] args = arg.Split(' ');
+        string[] args = CommandSplit(arg);
         IEnumerable<MethodInfo> commands = Assembly.GetCallingAssembly().GetTypes().SelectMany(t => t.GetMethods())
             .Where(m => m.GetCustomAttributes(typeof(cmd), false).Length > 0);
         foreach (MethodInfo command in commands)
@@ -83,7 +122,7 @@ public class consoleVII : MonoBehaviour
                 if (!cmd_info.cheat || cheats)
                 {
 
-                    command.Invoke(null, new object[] { args });
+                    command.Invoke(null, new object[] { args.Skip(1).ToArray() });
                     return;
                 }
                 else
@@ -155,9 +194,9 @@ public class consoleVII : MonoBehaviour
     [cmd("timescale", cheat = true, description = "changes the time scale")]
     public static void Timescale(string[] args)
     {
-        if (args.Length != 2)
+        if (args.Length != 1)
             consoleVII.instance.print("Usage: timescale <number>");
-        float timescale = float.Parse(args[1]);
+        float timescale = float.Parse(args[0]);
         Time.timeScale = timescale;
     }
 
@@ -167,17 +206,16 @@ public class consoleVII : MonoBehaviour
         consoleVII.log = "";
     }
 
-    [cmd("delete-lights", cheat = true, description = "WARNING: CANNOT BE REVERSED!")]
-    public static void Fullbright(string[] args)
+    [cmd("bind", description = "Binds a key to a command")]
+    public static void Bind(string[] args)
     {
-        foreach (Light l in FindObjectsOfType<Light>())
-        {
-            Destroy(l);
-        }
-        RenderSettings.ambientLight = new Color(1, 1, 1);
-        RenderSettings.reflectionIntensity = 0;
-        RenderSettings.fog = false;
-        consoleVII.instance.print("Lights deleted");
+        consoleVII.instance.binds[args[0]] = args[1];
+    }
+
+    [cmd("unbind", description = "Unbinds a key from commands")]
+    public static void Unbind(string[] args)
+    {
+        consoleVII.instance.binds.Remove(args[0]);
     }
 }
 
